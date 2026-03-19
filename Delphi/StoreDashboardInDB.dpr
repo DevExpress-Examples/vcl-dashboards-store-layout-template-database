39ff147867f9625c107b4eb8372ea984e99b4d74
Nick Volynkin <nikolay.volynkin@devexpress.com>
2026-03-19 15:00:09 +0200

    delphi: Rename example project

--- Delphi/StoreDashboardInDB.dpr
+++ Delphi/StoreDashboardInDB.dpr
@@ -1,16 +1,17 @@
-program DashboardTemplate;
+program StoreDashboardInDB;
 
 uses
   Vcl.Forms,
   uMainForm in 'uMainForm.pas' {MainForm},
   uData in 'uData.pas' {DataModule1: TDataModule},
-  DashboardTemplate.dxSettings in 'DashboardTemplate.dxSettings.pas';
+  StoreDashboardInDB.dxSettings in 'StoreDashboardInDB.dxSettings.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.Title := 'DevExpress VCL: Store Dashboard Layout in a Database';
   Application.CreateForm(TDataModule1, DataModule1);
   Application.CreateForm(TMainForm, MainForm);
   Application.Run;
