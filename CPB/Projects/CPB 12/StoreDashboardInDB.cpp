cd84497d20ae1b1ed2b6cf1529050eff57814368
Ilia Nenashev <ilia.nenashev@devexpress.com>
2026-07-07 10:07:28 +0300

    add project for CPB 12

---
+++ CPB/Projects/CPB 12/StoreDashboardInDB.cpp
@@ -0,0 +1,38 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+#include <tchar.h>
+//---------------------------------------------------------------------------
+USEFORM("..\..\Sources\uMainForm.cpp", MainForm);
+USEFORM("..\..\Sources\uData.cpp", DataModule1); /* TDataModule: File Type */
+//---------------------------------------------------------------------------
+int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
+{
+	try
+	{
+		Application->Initialize();
+		Application->MainFormOnTaskBar = true;
+		Application->Title = "DevExpress VCL: Store Dashboard Layout in a Database";
+		Application->CreateForm(__classid(TDataModule1), &DataModule1);
+		Application->CreateForm(__classid(TMainForm), &MainForm);
+		Application->Run();
+	}
+	catch (Exception &exception)
+	{
+		Application->ShowException(&exception);
+	}
+	catch (...)
+	{
+		try
+		{
+			throw Exception("");
+		}
+		catch (Exception &exception)
+		{
+			Application->ShowException(&exception);
+		}
+	}
+	return 0;
+}
+//---------------------------------------------------------------------------
